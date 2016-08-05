b79d3da72118c9d91d1c054245a7ece521ed87a9
Braunson Yager <braunson@gmail.com>
2016-08-04 22:04:11 -0400

    Added Clockwork, Pivot Table, Fixed Models
    @julianyager it's ready!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,11 +9,29 @@ use App\Http\Requests;
 
 class HomeController extends Controller
 {
-    public function index()
+	public function index()
 	{
+		// Get the first user
 		$user = User::all()->first();
 
-		dd($user->ads[0]);
+		// Get the first ads for this user
+		$ad = $user->ads->first();
+
+		echo "Ad Author: " . $ad->user->name . '<br>';
+		echo "Ad Title: " . $ad->title . '<br>';
+		echo "Ad Description: " . $ad->description . '<br><br>';
+
+		// TEST V1: Die and dump all instruments.
+		// dd($ad->instruments);
+
+		// TEST V2: Echo out the instruments and die
+		$i = 1;
+		foreach ($ad->instruments as $instrument) {
+			echo "Instrument #" . $i . " is: " . $instrument->name . '<br>';
+			$i++;
+		}
+		die();
+
 
 		return view('welcome')->withUser($user);
 	}
